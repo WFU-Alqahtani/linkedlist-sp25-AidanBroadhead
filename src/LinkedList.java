05f4c2ec01cd6cb498bfdf4e437f401dc07f3806
AidanBroadhead <broaaj23@wfu.edu>
2025-03-05 15:05:30 -0500

    Deleted and inserted nodes

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,4 +1,5 @@
 import java . util .*;
+import java.util.Scanner;
 
 class Node{
     int data;
@@ -24,6 +25,7 @@ public class LinkedList {
             curr = curr.next;
         }
 
+
         //print
         curr = head;
         while (curr != null) {
@@ -31,6 +33,46 @@ public class LinkedList {
             curr = curr.next;
         }
 
-    }
 
+        Scanner scnr = new Scanner(System.in);
+
+        // ask user which node they want to delete
+        System.out.println("Enter x you want to delete: ");
+        int x = scnr.nextInt();
+
+        curr = head.next;
+        Node prev = head;
+
+        // while current is not null, check to see if node equals user inputted value, then delete that node
+        while (curr != null) {
+            if (curr.data == x) {
+                prev.next = curr.next;
+            }
+            prev = prev.next;
+            curr = curr.next;
+        }
+
+        // insert node at head
+        Node temp = head;
+        Node newNode = new Node(20, null);
+        newNode.next = temp;
+
+        // insert node at end
+        curr = newNode;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+
+        Node endNode = new Node(60, null);
+        curr.next = endNode;
+
+
+        //print list with inserted and deleted nodes
+        curr = newNode;
+        while (curr != null) {
+            System.out.println(curr.data);
+            curr = curr.next;
+        }
+
+    }
 }
